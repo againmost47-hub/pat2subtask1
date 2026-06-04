1899eaee7e457518b88d48dc0557e898a3be23e4
againmost47-hub <againmost47@gmail.com>
2026-06-04 10:00:21 +0200

    Add morse code character mapping: Implement to_Morse function with A-Z character conversion table

--- morse_code.cpp
+++ morse_code.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+string to_Morse(char c) {
+    switch (c) {
+    case 'A': return ".-";
+    case 'B': return "-...";
+    case 'C': return "-.-.";
+    case 'D': return "-..";
+    case 'E': return ".";
+    case 'F': return "..-.";
+    case 'G': return "--.";
+    case 'H': return "....";
+    case 'I': return "..";
+    case 'J': return ".---";
+    case 'K': return "-.-";
+    case 'L': return ".-..";
+    case 'M': return "--";
+    case 'N': return "-.";
+    case 'O': return "---";
+    case 'P': return ".--.";
+    case 'Q': return "--.-";
+    case 'R': return ".-.";
+    case 'S': return "...";
+    case 'T': return "-";
+    case 'U': return "..-";
+    case 'V': return "...-";
+    case 'W': return ".--";
+    case 'X': return "-..-";
+    case 'Y': return "-.--";
+    case 'Z': return "--..";
+    default: return "";
+    }
+}
+
 int main() {
-    cout << "Morse Code Converter" << endl;
+    string message;
+    cout << "Enter a message: ";
+    getline(cin, message);
     return 0;
 }
